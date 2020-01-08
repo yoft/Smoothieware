7cf28bd97d1a6215e1b2de74b7b32725aee97404
Shaun <Shaun.halley.3@gmail.com>
2020-01-08 18:41:08 +1100

    Correct indentation

--- src/modules/tools/toolmanager/ToolManager.cpp
+++ src/modules/tools/toolmanager/ToolManager.cpp
@@ -3,7 +3,7 @@
       Smoothie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
       Smoothie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
       You should have received a copy of the GNU General Public License along with Smoothie. If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 #include "libs/Module.h"
 #include "libs/Kernel.h"
@@ -33,12 +33,12 @@ ToolManager::ToolManager()
 }
 
 uint16_t *ToolManager::get_active_tool_name() {
-	current_tool_name=this->tools[active_tool]->get_name();
-	return &current_tool_name;
+    current_tool_name=this->tools[active_tool]->get_name();
+    return &current_tool_name;
 }
 
 const float *ToolManager::get_active_tool_offset() {
-	return this->tools[active_tool]->get_offset();
+    return this->tools[active_tool]->get_offset();
 }
 
 void ToolManager::on_module_loaded()
@@ -56,24 +56,24 @@ void ToolManager::on_gcode_received(void *argument)
     if( gcode->has_letter('T') ) {
         int new_tool = gcode->get_value('T');
         if(new_tool >= (int)this->tools.size() || new_tool < 0) {
-			// invalid tool
-			char buf[32]; // should be big enough for any status
-			int n = snprintf(buf, sizeof(buf), "T%d invalid tool ", new_tool);
-			gcode->txt_after_ok.append(buf, n);
-		}else{
-			this->next_tool=new_tool;
-		}
+            // invalid tool
+            char buf[32]; // should be big enough for any status
+            int n = snprintf(buf, sizeof(buf), "T%d invalid tool ", new_tool);
+            gcode->txt_after_ok.append(buf, n);
+        }else{
+            this->next_tool=new_tool;
+        }
     }
     if (gcode->has_m && gcode->m==6) {
-    	if(this->next_tool >= (int)this->tools.size() || this->next_tool < 0) {
-			// invalid tool
-			char buf[32]; // should be big enough for any status
-			int n = snprintf(buf, sizeof(buf), "T%d invalid tool ", this->next_tool);
-			gcode->txt_after_ok.append(buf, n);
+        if(this->next_tool >= (int)this->tools.size() || this->next_tool < 0) {
+            // invalid tool
+            char buf[32]; // should be big enough for any status
+            int n = snprintf(buf, sizeof(buf), "T%d invalid tool ", this->next_tool);
+            gcode->txt_after_ok.append(buf, n);
 
-		}else if (this->next_tool != this->active_tool) {
-			this->change_tool();
-		}
+        }else if (this->next_tool != this->active_tool) {
+            this->change_tool();
+        }
     }
 }
 
@@ -137,19 +137,19 @@ void ToolManager::add_tool(Tool* tool_to_add)
 // Add a tool to the tool list
 void ToolManager::change_tool()
 {
-	// We must wait for an empty queue before we can disable the current tool
-	THEKERNEL->conveyor->wait_for_idle();
-	this->tools[active_tool]->deselect();
-	THEKERNEL->conveyor->wait_for_idle();
-	this->active_tool = this->next_tool;
-	//this->current_tool_name = this->tools[active_tool]->get_name();
+    // We must wait for an empty queue before we can disable the current tool
+    THEKERNEL->conveyor->wait_for_idle();
+    this->tools[active_tool]->deselect();
+    THEKERNEL->conveyor->wait_for_idle();
+    this->active_tool = this->next_tool;
+    //this->current_tool_name = this->tools[active_tool]->get_name();
 
-	//send new_tool_offsets to robot
-	//const float *new_tool_offset = tools[next_tool]->get_offset();
-	THEROBOT->setToolOffset(this->get_active_tool_offset());
+    //send new_tool_offsets to robot
+    //const float *new_tool_offset = tools[next_tool]->get_offset();
+    THEROBOT->setToolOffset(this->get_active_tool_offset());
 
-	this->tools[active_tool]->select();
-	THEKERNEL->conveyor->wait_for_idle();
+    this->tools[active_tool]->select();
+    THEKERNEL->conveyor->wait_for_idle();
 }
 
 
